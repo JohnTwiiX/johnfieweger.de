1feaeacfe7f38be5aaae650c60778dae1eba3e2c
John Fieweger <john.fieweger98@gmail.com>
2022-05-25 12:07:49 +0200

    added img slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'johnfieweger';
+export class AppComponent implements OnInit{
+  images = ['berufsschule.jpg', 'bundeswehr.jpg', 'code.jpg'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit(): void {
+    this.updateImages();
+  }
+
+  updateImages(){
+
+    setInterval(()=>{
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length; // % Modulo rechnet den Rest aus.. heißt alle 8sek 0, 1, 2.. 0, 1, 2.. fängt immer wieder von vorne an
+      this.showImage = false;
+
+      setTimeout(()=>{
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+
+  }
 }
