417c6bfde2f41d8640cddd9b2f27c873cdbff475
John Fieweger <john.fieweger98@gmail.com>
2022-05-30 13:49:22 +0200

    added home-screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SlideshowComponent } from './slideshow/slideshow.component';
 import { MenuComponent } from './menu/menu.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent,
     MenuComponent,
     ImprintComponent,
-    DataProtectionComponent
+    DataProtectionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
