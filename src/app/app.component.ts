e5cd84413e360d89007a34972494e1a3459d0a7e
John Fieweger <john.fieweger98@gmail.com>
2024-11-30 21:11:25 +0100

    refactor: upgrade to angular v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import * as AOS from 'aos';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router } from '@angular/router';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'johnfieweger';
+export class AppComponent implements OnInit, AfterViewInit {
+  title = 'Portfolio';
+  constructor(public router: Router) { }
 
-  constructor(public router: Router){}
+  ngOnInit(): void {
+    AOS.init({
+      duration: 750,
+      delay: 150,
+    });
+  }
 
-  ngOnInit() {
-    AOS.init();
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      AOS.refresh()
+    }, 500)
   }
 
 }
--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
   selector: 'app-imprint',
   templateUrl: './imprint.component.html',
-  styleUrls: ['./imprint.component.scss']
+  styleUrls: ['./imprint.component.scss'],
+  imports: [FooterComponent, MenuComponent]
 })
 export class ImprintComponent implements OnInit {
 
